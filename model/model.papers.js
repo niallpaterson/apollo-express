d41bf697d3ca216b5ebc86992df319d32f13b112
NiallJP <59969425+niallpaterson@users.noreply.github.com>
2021-02-05 07:58:13 +0000

    feat: add model

---
+++ model/model.papers.js
@@ -0,0 +1,54 @@
+if (!process.env.GOOGLE_SERVICE_KEY) {
+  throw new Error('Environment does not contain google service key.');
+}
+
+const firebase = require('firebase-admin');
+const serviceKey = JSON.parse(process.env.GOOGLE_SERVICE_KEY);
+const credential = firebase.credential.cert(serviceKey);
+firebase.initializeApp({ credential });
+
+const db = firebase.firestore();
+
+const collection = 'papers';
+
+// use more efficient method
+
+const find = (id) =>
+  db
+    .collection(collection)
+    .get()
+    .then((snapshot) =>
+      snapshot.docs.map((doc) => doc.data()).filter((datum) => datum.id === id)[0]
+    );
+
+const findAll = () =>
+  db
+    .collection(collection)
+    .get()
+    .then((snapshot) => snapshot.docs.map((doc) => doc.data()));
+
+const create = (id, title, published) =>
+  db
+    .collection(collection)
+    .doc(id)
+    .set({ title, id, published })
+    .then(() => ({
+      title,
+      id,
+      published,
+    }))
+    .catch((e) => {
+      throw e;
+    });
+
+const destroy = (id) =>
+  db
+    .collection(collection)
+    .doc(id)
+    .delete()
+    .then(() => `${id} successfully deleted`)
+    .catch((e) => {
+      throw e;
+    });
+
+module.exports = { findAll, find, create, destroy };
